a4e62f390eec559ffbb39ce6524f29ca0732fa71
Arief Badal <1042170@hr.nl>
2022-10-26 18:33:40 +0200

    Add  portofolio objects

---
+++ src/portofolio.js
@@ -0,0 +1,13 @@
+const header = {
+    homepage: '',
+    title: 'AB.'
+};
+
+const about = {
+    name: 'Arief Badal',
+    role: 'Software developer student',
+    description: 'Hi, my name is Arief I currently do a Software developer education at Rotterdam Academy',
+
+}
+
+console.log(about.name)
