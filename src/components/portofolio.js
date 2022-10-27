3116239c245815e574e623ac2855139ec7c1c807
Arief Badal <1042170@hr.nl>
2022-10-27 20:00:29 +0200

    Add github, linkedin links and skilss

--- src/components/portofolio.js
+++ src/components/portofolio.js
@@ -6,6 +6,24 @@ const header = {
 export const about = {
     name: 'Arief Badal',
     role: 'Software developer student',
-    description: 'Hi, my name is Arief I currently do a Software developer education at Rotterdam Academy'
+    description: 'Hi, my name is Arief I currently do a Software developer education at Rotterdam Academy',
+    social: {
+        linkedin: 'https://www.Linkedin.nl',
+        github: 'https://Github.com'
+    },
 };
 
+
+export const projects = {
+    project1: 'Astroids',
+    project2: 'Darthub',
+    project3: 'Darthub mobile app',
+    project4: '360 feedback tool',
+    project5: 'particle system',
+    
+}
+export const skills = [
+    'Teamplayer',
+    'Python',
+    'Javascript'
+]
