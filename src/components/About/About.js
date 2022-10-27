8fa566bf4c74f3e571ad97c815158d9404981249
Arief Badal <1042170@hr.nl>
2022-10-27 19:44:50 +0200

    Add github and linkedin link

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,12 +3,17 @@ import './About.css';
 
 
 function About() {
-    const {name, role, description} = about
+    const {name, role, description, social:{
+        linkedin, github
+    }} = about
     return ( 
         <div className='about-section'>
             <h2>{name}</h2>
             <h2>{role}</h2>
             <h3>{description}</h3>
+            <p><a href={linkedin}>Linkedin</a></p>
+            <p><a href={github}>Github</a></p>
+            
         </div>
      );
 }
