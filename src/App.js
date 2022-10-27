961ee9ecc10b63de731281bc7708a7a7397e6fdb
Arief Badal <1042170@hr.nl>
2022-10-27 20:00:49 +0200

    Add Skills component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import About from './components/About/About';
+import Skills from './components/Skills/Skills';
 
 // about details tonen op scherm 26-okt
 function App() {
   return (
     <div className='App'>
       <About/>
+      <Skills/>
     </div>
     
   );
