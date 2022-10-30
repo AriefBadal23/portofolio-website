8c09eb21d36919d386e427252aaeb615cc9616ab
Arief Badal <1042170@hr.nl>
2022-10-30 15:48:37 +0100

    import header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import About from './components/About/About';
 import Skills from './components/Skills/Skills';
+import Header from './components/header/header';
 
 // about details tonen op scherm 26-okt
 function App() {
   return (
     <div className='App'>
+      <Header/>
       <About/>
       <Skills/>
     </div>
