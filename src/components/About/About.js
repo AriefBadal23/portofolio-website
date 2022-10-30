69b6d0621f4229bcfe20fa8dcd4a8bc4dbbd88af
Arief Badal <1042170@hr.nl>
2022-10-30 15:48:10 +0100

    Add github and linkedin icon

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,21 +1,21 @@
-import {about} from '../portofolio'
+import { about } from '../portofolio';
 import './About.css';
-
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 function About() {
-    const {name, role, description, social:{
+    const { name, role, description, social: {
         linkedin, github
-    }} = about
-    return ( 
+    } } = about;
+    return (
         <div className='about-section'>
-            <h2>{name}</h2>
-            <h2>{role}</h2>
+            <h2>Hi, I am <h2 className='about-name'>{name}</h2></h2>
+            <h2 className='about-role'>A {role}</h2>
             <h3>{description}</h3>
-            <p><a href={linkedin}>Linkedin</a></p>
-            <p><a href={github}>Github</a></p>
-            
+            <LinkedInIcon /><a href={linkedin}></a>
+            <GitHubIcon /><a href={github}></a>
         </div>
-     );
+    );
 }
 
-export default About
+export default About;
