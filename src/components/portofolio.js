3e5f63e78534e659ef0ecc218200c34d95b04004
Arief Badal <1042170@hr.nl>
2022-11-06 22:43:45 +0100

    Formatted file and changed projects object

--- src/components/portofolio.js
+++ src/components/portofolio.js
@@ -1,29 +1,29 @@
 export const header = {
-    homepage: '',
-    title: 'AB.'
+  homepage: "",
+  title: "AB.",
 };
 
 export const about = {
-    name: 'Arief Badal',
-    role: 'Software developer student',
-    description: 'Hi, my name is Arief I currently do a Software developer education at Rotterdam Academy',
-    social: {
-        linkedin: 'https://www.linkedin.com/',
-        github: 'https://Github.com'
-    },
+  name: "Arief Badal",
+  role: "Software developer student",
+  description:
+    "Hi, my name is Arief I currently do a Software developer education at Rotterdam Academy",
+  social: {
+    linkedin: "https://www.linkedin.com/",
+    github: "https://Github.com",
+  },
 };
 
-
-export const projects = {
-    project1: 'Astroids',
-    project2: 'Darthub',
-    project3: 'Darthub mobile app',
-    project4: '360 feedback tool',
-    project5: 'particle system',
-    
-}
-export const skills = [
-    'Teamplayer',
-    'Python',
-    'Javascript'
-]
+export const projects = [
+ {
+      name: "Astroid",
+      description: "A astrpoid game made in Pygame",
+      codestack: "Python 3",
+    },
+  {
+    name: "Darthub",
+    description: "A simple website",
+    codestack: "HTML",
+  },
+];
+export const skills = ["Teamplayer", "Python", "Javascript"];
