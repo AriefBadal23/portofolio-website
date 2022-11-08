8fe2e458da435e2f467881d7a1fda2cfbe077917
Arief Badal <1042170@hr.nl>
2022-11-08 19:59:05 +0100

    Change style of About component

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -9,12 +9,15 @@ function About() {
     } } = about;
     return (
         <div className='about-section'>
-            <h1>Hi, I am  <span className='about-name'>{name}.</span></h1>
-            <h2 className='about-role'>A {role}</h2>
-            <p>{description}</p>
+            <h1 id='about-intro'>Hi, I am  <span className='about-name'>{name}.</span></h1>
+            <h2 className='about-role'>A {role}.</h2>
+            <p>{description}.</p>
             {/* <button><a href='#'>Resume</a></button> */}
-            <a href={github}><GitHubIcon /></a>
-            <a href={linkedin}><LinkedInIcon /></a>
+            <div className='about-buttons'>
+                <a href={github}><GitHubIcon /></a>
+                <a href={linkedin}><LinkedInIcon /></a>
+            </div>
+            
         </div>
     );
 }
