704f4d26d42093d5f2531c6cc00c2fb29b45dcfb
Arief Badal <1042170@hr.nl>
2022-11-10 19:42:11 +0100

    Refactor of the project contaner card

--- src/components/Projectcontainer/projectcontainer.js
+++ src/components/Projectcontainer/projectcontainer.js
@@ -8,12 +8,11 @@ function ProjectContainer({ project }) {
     return (
         <div id="container">
             <div className="card">
-                <img src="https://t4.ftcdn.net/jpg/02/20/95/83/240_F_220958300_zORapGHwkTfw2XIeS2mQVf0cdcwMMwxB.jpg" alt=""></img>
+                <img src={`images/${project.image}`} alt={project.description}></img>
                 <span className="tag">{project.codestack}</span>
-                <span className="tag">Pygame</span>
                 <div className="name">{project.name}</div>
                 <p>{project.description}</p>
-                <Button variant="outlined"><GitHubIcon></GitHubIcon></Button>
+                <Button variant="outlined"><GitHubIcon/></Button>
             </div>
         </div>
     );
