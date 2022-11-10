b86a2de9e1b1b9f18218452bcdd47cb779abfe6f
Arief Badal <1042170@hr.nl>
2022-11-10 19:24:14 +0100

    Refactor buttons for linkedin and github

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,6 +2,8 @@ import { about } from '../portofolio';
 import './About.css';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
+import Button from '@mui/material/Button'
+
 
 function About() {
     const { name, role, description, social: {
@@ -14,8 +16,9 @@ function About() {
             <p>{description}.</p>
             {/* <button><a href='#'>Resume</a></button> */}
             <div className='about-buttons'>
-                <a href={github}><GitHubIcon /></a>
-                <a href={linkedin}><LinkedInIcon /></a>
+                <Button variant='outlined'>GitHub<a href={github}><GitHubIcon /></a></Button>
+                <Button variant='outlined'>LinkedIn<a href={linkedin}><LinkedInIcon /></a></Button>
+                
             </div>
             
         </div>
