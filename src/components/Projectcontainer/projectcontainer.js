c6cda8813a0f65f6e263090adaf37bc8d2142e83
Arief Badal <1042170@hr.nl>
2022-11-10 19:09:55 +0100

    Add project container with cards  for projects

--- src/components/Projectcontainer/projectcontainer.js
+++ src/components/Projectcontainer/projectcontainer.js
@@ -1,13 +1,22 @@
 // {project} omdat dit beter is dan props.project
-import './projectcontainer.css'
-function ProjectContainer({project}) {
+import "./projectcontainer.css";
+import GitHubIcon from '@mui/icons-material/GitHub';
+import Button from '@mui/material/Button'
+
+
+function ProjectContainer({ project }) {
     return (
-        <div className="project-container">
-                <p>{project.name}</p>
+        <div id="container">
+            <div className="card">
+                <img src="https://t4.ftcdn.net/jpg/02/20/95/83/240_F_220958300_zORapGHwkTfw2XIeS2mQVf0cdcwMMwxB.jpg" alt=""></img>
+                <span className="tag">{project.codestack}</span>
+                <span className="tag">Pygame</span>
+                <div className="name">{project.name}</div>
                 <p>{project.description}</p>
-                <p>{project.codestack}</p>
+                <Button variant="outlined"><GitHubIcon></GitHubIcon></Button>
+            </div>
         </div>
-     );
+    );
 }
 
-export default ProjectContainer;    
+export default ProjectContainer;
