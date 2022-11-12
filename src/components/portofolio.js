f3b629a168b3238ecf81bdd13725f114ff819ae2
Arief Badal <1042170@hr.nl>
2022-11-12 14:03:40 +0100

    Refactor of project object

--- src/components/portofolio.js
+++ src/components/portofolio.js
@@ -7,7 +7,8 @@ export const about = {
   name: "Arief Badal",
   role: "Software developer student",
   description:
-    "Hi, my name is Arief I currently do a Software developer education at Rotterdam Academy",
+    "Hi, my name is Arief I currently do a Software developer education at Rotterdam Academy.",
+    //  Here I have learned to program with different languages like Python, Javascript and to work with the Scrum methodology",
   social: {
     linkedin: "https://www.linkedin.com/",
     github: "https://Github.com",
@@ -16,14 +17,17 @@ export const about = {
 
 export const projects = [
  {
-      name: "Astroid",
-      description: "A astrpoid game made in Pygame",
+      name: "Astroids",
+      description: "The retro game Astroids made with the library Pygame",
       codestack: "Python 3",
+      image: "astroids.png",
     },
-  {
-    name: "Darthub",
-    description: "A simple website",
-    codestack: "HTML",
-  },
-];
+ {
+      name: "Dartsbond mobile app",
+      description: "A mobile app created with Ionic React to let visitors view dartevents",
+      codestack: "Ionic React",
+      image: "Dartbonds_app.png",
+    }
+  ]
+ 
 export const skills = ["Teamplayer", "Python", "Javascript"];
