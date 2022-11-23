77d5b7022af35a6319fa0be9bd4aa30714f4f186
Arief Badal <1042170@hr.nl>
2022-11-23 20:37:13 +0100

    Refactored styling of skills and added skillbar component

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,17 +1,21 @@
-import { skills } from "../portofolio";
+import Skillbar from "../../Skillbar/Skillbar";
+import {skills } from "../portofolio";
 import "./Skills.css";
 
 function Skills() {
+  const [Python, Javascript, MySQL, Flask] = skills
   return (
     // changed classname to id to make the anchor work in the navbar when clicked
     // the id attribute is not unique! (seen error in the console)
     <div className="skills-container">
         <h1 id="skills-header">Skills</h1>
-      <span>
-        {skills.map((skill) => (
-          <p id="skill">{skill}</p>
-        ))}
-      </span>
+        <div className="skill">
+         <Skillbar skill={Python}/>
+         
+         <Skillbar skill={MySQL}/>
+         <Skillbar skill={Javascript}/>
+         <Skillbar skill={Flask}/>
+  	    </div>
     </div>
   );
 }
