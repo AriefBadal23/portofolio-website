fa41f6a58f28da039960a208ee44427c49a651d4
Arief Badal <1042170@hr.nl>
2022-11-23 20:35:01 +0100

    Refactor content and styling of About component

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -13,11 +13,12 @@ function About() {
         <div className='about-section'>
             <h1 id='about-intro'>Hi, I am  <span className='about-name'>{name}.</span></h1>
             <h2 className='about-role'>A {role}.</h2>
-            <p>{description}.</p>
+            <p>Hi, my name is Arief I currently do a Software developer education at Rotterdam Academy</p>
+            <p> Here I have learned to program with different languages like Python, Javascript and to work with the Scrum methodology</p>
             {/* <button><a href='#'>Resume</a></button> */}
             <div className='about-buttons'>
-                <Button variant='outlined'>GitHub<a href={github}><GitHubIcon /></a></Button>
-                <Button variant='outlined'>LinkedIn<a href={linkedin}><LinkedInIcon /></a></Button>
+                <a href={github}><Button variant='outlined'>GitHub<GitHubIcon /></Button></a>
+                <a href={linkedin}><Button variant='outlined'>LinkedIn<LinkedInIcon /></Button></a>
                 
             </div>
             
