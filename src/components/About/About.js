05f2ededb4b33e77ace8dca21989bc7afbe0f287
Arief Badal <1042170@hr.nl>
2022-11-24 21:03:01 +0100

    Refactor about me description

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,15 +6,20 @@ import Button from '@mui/material/Button'
 
 
 function About() {
-    const { name, role, description, social: {
+    const { name, role, social: {
         linkedin, github
     } } = about;
     return (
         <div className='about-section'>
             <h1 id='about-intro'>Hi, I am  <span className='about-name'>{name}.</span></h1>
             <h2 className='about-role'>A {role}.</h2>
-            <p>Hi, my name is Arief I currently do a Software developer education at Rotterdam Academy</p>
-            <p> Here I have learned to program with different languages like Python, Javascript and to work with the Scrum methodology</p>
+            <span className='about-description'>
+                <p>Hi, my name is Arief I currently do a Software developer education at Rotterdam Academy.</p>
+                <p>Here I have learned to program with different languages like Python, Javascript and to </p>
+                <p>work with the Scrum methodology. We used this while working on different coding projects.</p>
+                <p>On my portofolio you can find the projects I have worked on with my schoolprojects and some</p>
+                <p>personal projects as well.</p>
+            </span>
             {/* <button><a href='#'>Resume</a></button> */}
             <div className='about-buttons'>
                 <a href={github}><Button variant='outlined'>GitHub<GitHubIcon /></Button></a>
