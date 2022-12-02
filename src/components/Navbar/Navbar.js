7eb0014f3443ca7a9807c1fbca022ca58f3b4d21
Arief Badal <1042170@hr.nl>
2022-12-02 21:04:35 +0100

    Change links in  a  href tags

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,13 +5,13 @@ function Navbar() {
         <nav className="Navbar">
             <ul>
                 {/* TODO: #container has changed to project-header so the anchor will work */}
-            <a href='#container'><item className='Navbar-item'>
+            <a href='#demo-container'><item className='Navbar-item'>
                     Projects
                     </item></a>
             <a href='#skills-header'><item className='Navbar-item'>
                     Skills
                     </item></a>
-            <a href='#contact'><item className='Navbar-item'>
+            <a href='#contact-section'><item className='Navbar-item'>
                     Contact
                     </item></a>
             </ul>
