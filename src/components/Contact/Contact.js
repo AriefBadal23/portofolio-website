265aa3ab29f0a76796c952bd7f4151533f712266
Arief Badal <1042170@hr.nl>
2022-12-02 11:12:31 +0100

    Add Contact component

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,11 @@
+import './Contact.css'
+function Contact() {
+    return ( 
+        <div className="contact-section">
+            <h1>Contact</h1>
+            <p>If you want to contact me feel free to send me a message via <a href='.about-section'>linkedin.</a></p>
+        </div>
+     );
+}
+
+export default Contact;
