e92f1d9b6d144d790bf1091d5187008a40461440
Arief Badal <1042170@hr.nl>
2022-12-02 20:52:19 +0100

    Change import of header component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import About from './components/About/About';
 import Skills from './components/Skills/Skills';
-import Header from './components/Header/header';
+import Header from './components/header/header';
 import Project from './components/Projects/projects';
 import Navbar from './components/Navbar/Navbar';
 import Contact from './components/Contact/Contact'
