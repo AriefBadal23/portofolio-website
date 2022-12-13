b1fed1d4d8717f400a1ff75d07684e1745590336
Arief Badal <1042170@hr.nl>
2022-12-13 16:40:13 +0100

    Small refactor to fix bug of not centered text

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -10,9 +10,11 @@ function About() {
         linkedin, github
     } } = about;
     return (
-        <div className='about-section'>
-            <h1 id='about-intro'>Hi, I am  <span id='about-name'>{name}.</span></h1>
-            <h2 className='about-role'>A {role}.</h2>
+        <div>
+            <span className='about-section'>
+                <h1 id='about-name'>Hi, I am  <span id='about-name'>{name}.</span></h1>
+                <h2>A {role}.</h2>
+
             <span className='about-description'>
                 <p>Hi, my name is Arief I currently do a Software developer education at Rotterdam Academy.</p>
                 <p>Here I have learned to program with different languages like Python, Javascript and to </p>
@@ -20,13 +22,14 @@ function About() {
                 <p>On my portofolio you can find the projects I have worked on with my schoolprojects and some</p>
                 <p>personal projects as well.</p>
             </span>
+
             {/* <button><a href='#'>Resume</a></button> */}
             <div className='about-buttons'>
                 <a href={github}><Button variant='outlined'>GitHub<GitHubIcon /></Button></a>
                 <a href={linkedin}><Button variant='outlined'>LinkedIn<LinkedInIcon /></Button></a>
                 
             </div>
-            
+            </span>
         </div>
     );
 }
