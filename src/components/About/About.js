b864d6934f083c883d577e8209d685215e290ae1
Arief Badal <1042170@hr.nl>
2022-12-13 16:59:38 +0100

    Small refactor to fix styling back to original

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,7 +2,7 @@ import { about } from '../portofolio';
 import './About.css';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
-import Button from '@mui/material/Button'
+import Button from '@mui/material/Button';
 
 
 function About() {
@@ -12,23 +12,22 @@ function About() {
     return (
         <div>
             <span className='about-section'>
-                <h1 id='about-intro'>Hi, I am  <span id='name'>{name}.</span></h1>
+                <h1><span id='about-intro'>Hi, I am  </span><span id='about-name'>{name}.</span></h1>
                 <h2 id='about-role'>A {role}.</h2>
+                <span className='about-description'>
+                    <p>Hi, my name is Arief I currently do a Software developer education at Rotterdam Academy.</p>
+                    <p>Here I have learned to program with different languages like Python, Javascript and to </p>
+                    <p>work with the Scrum methodology. We used this while working on different coding projects.</p>
+                    <p>On my portofolio you can find the projects I have worked on with my schoolprojects and some</p>
+                    <p>personal projects as well.</p>
+                </span>
 
-            <span className='about-description'>
-                <p>Hi, my name is Arief I currently do a Software developer education at Rotterdam Academy.</p>
-                <p>Here I have learned to program with different languages like Python, Javascript and to </p>
-                <p>work with the Scrum methodology. We used this while working on different coding projects.</p>
-                <p>On my portofolio you can find the projects I have worked on with my schoolprojects and some</p>
-                <p>personal projects as well.</p>
-            </span>
+                {/* <button><a href='#'>Resume</a></button> */}
+                <div className='about-buttons'>
+                    <a href={github}><Button variant='outlined'>GitHub<GitHubIcon /></Button></a>
+                    <a href={linkedin}><Button variant='outlined'>LinkedIn<LinkedInIcon /></Button></a>
 
-            {/* <button><a href='#'>Resume</a></button> */}
-            <div className='about-buttons'>
-                <a href={github}><Button variant='outlined'>GitHub<GitHubIcon /></Button></a>
-                <a href={linkedin}><Button variant='outlined'>LinkedIn<LinkedInIcon /></Button></a>
-                
-            </div>
+                </div>
             </span>
         </div>
     );
