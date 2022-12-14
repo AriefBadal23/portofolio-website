e4296fb8ee90e64bfdaa16582f2b5380182e6b26
Arief Badal <1042170@hr.nl>
2022-12-14 19:36:29 +0100

    Fix bug of ancho ahref link

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -3,7 +3,7 @@ function Contact() {
     return ( 
         <div id="contact-section">
             <h1>Contact</h1>
-            <p>If you want to contact me feel free to send me a message via <a href='.about-section'>linkedin.</a></p>
+            <p>If you want to contact me feel free to send me a message via <a href='#about-role'>linkedin.</a></p>
         </div>
      );
 }
