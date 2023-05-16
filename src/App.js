273b225474d3d3d3385518df486f6725df680e79
Arief Badal <ariefbadal21@outlook.com>
2023-05-16 19:16:29 +0200

    move components folder again

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import About from '../components/About/About';
-import Skills from '../components/Skills/Skills';
-import Header from '../components/header/header';
-import Project from '../components/Projects/projects';
-import Navbar from '../components/Navbar/Navbar';
-import Contact from '../components/Contact/Contact';
+import About from './components/About/About';
+import Skills from './components/Skills/Skills';
+import Header from './components/header/header';
+import Project from './components/Projects/projects';
+import Navbar from './components/Navbar/Navbar';
+import Contact from './components/Contact/Contact';
 
 // about details tonen op scherm 26-okt
 function App() {
