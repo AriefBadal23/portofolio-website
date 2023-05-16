fb51a25801151d59a581890709c30ed4970955e1
Arief Badal <ariefbadal21@outlook.com>
2023-05-16 19:02:59 +0200

    change filename and move files

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import About from '../components/About/About';
+import Skills from '../components/Skills/Skills';
+import Header from '../components/header/header';
+import Project from '../components/Projects/projects';
+import Navbar from '../components/Navbar/Navbar';
+import Contact from '../components/Contact/Contact';
+
+// about details tonen op scherm 26-okt
+function App() {
+  return (
+    <div className='App'>
+      <Navbar/>
+      <Header/>
+      <About/>
+      <Project/>
+      <Skills/>
+      <Contact/>
+    </div>
+    
+  );
+}
+
+export default App;
