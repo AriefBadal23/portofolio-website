34bd9d2173e3ad75d71d0eb6d4f2b220b2a240c8
Arief Badal <ariefbadal21@outlook.com>
2023-05-16 18:51:13 +0200

    Move files to the root for gh-pages

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import Skills from './components/Skills/Skills';
 import Header from './components/header/header';
 import Project from './components/Projects/projects';
 import Navbar from './components/Navbar/Navbar';
-import Contact from './components/Contact/Contact'
+import Contact from './components/Contact/Contact';
 
 // about details tonen op scherm 26-okt
 function App() {
